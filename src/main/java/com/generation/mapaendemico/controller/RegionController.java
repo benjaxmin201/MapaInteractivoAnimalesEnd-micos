ff256a0f765d270cc550c15610b02f06b5835ccc
RemiAspergay <168590648+RemiAspergay@users.noreply.github.com>
2024-06-21 10:08:45 -0400

    añadidos metodos a regionservice y regioncontroller

--- src/main/java/com/generation/mapaendemico/controller/RegionController.java
+++ src/main/java/com/generation/mapaendemico/controller/RegionController.java
@@ -1,9 +1,13 @@
 package com.generation.mapaendemico.controller;
 
+import com.generation.mapaendemico.service.RegionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/regiones")
 public class RegionController {
+    @Autowired
+    private RegionService regionService;
 }
--- src/main/java/com/generation/mapaendemico/service/RegionService.java
+++ src/main/java/com/generation/mapaendemico/service/RegionService.java
@@ -1,7 +1,16 @@
 package com.generation.mapaendemico.service;
 
+import com.generation.mapaendemico.models.Region;
+import com.generation.mapaendemico.repository.RegionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RegionService {
+    @Autowired
+    private RegionRepository regionRepository;
+
+    public Region getRegionById(Integer id) {
+        return regionRepository.getReferenceById(id);
+    }
 }
