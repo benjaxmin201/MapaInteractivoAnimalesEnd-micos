291c843f836521fd28d21c80d2caf590f41b8aa5
Rafael <errecuevas.a@gmail.com>
2024-06-21 11:13:26 -0400

    MIAE30

--- src/main/java/com/generation/mapaendemico/service/FaunaService.java
+++ src/main/java/com/generation/mapaendemico/service/FaunaService.java
@@ -1,7 +1,16 @@
 package com.generation.mapaendemico.service;
 
+import com.generation.mapaendemico.models.Fauna;
+import com.generation.mapaendemico.repository.FaunaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FaunaService {
+    @Autowired
+    private FaunaRepository faunarepository;
+
+    public Fauna getFaunaById(Integer id) {
+        return faunarepository.getReferenceById(id);
+    }
 }
