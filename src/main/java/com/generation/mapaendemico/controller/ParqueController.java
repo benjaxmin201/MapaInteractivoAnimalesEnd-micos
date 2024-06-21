c92853cb0a40433180e6937945dea1a596533181
Paula <paula.vargas.cist@gmail.com>
2024-06-21 08:18:08 -0600

    1 commit Pau

--- src/main/java/com/generation/mapaendemico/controller/ParqueController.java
+++ src/main/java/com/generation/mapaendemico/controller/ParqueController.java
@@ -1,9 +1,13 @@
 package com.generation.mapaendemico.controller;
 
+import com.generation.mapaendemico.service.ParqueService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/parques")
 public class ParqueController {
+    @Autowired
+    private ParqueService parqueService;
 }
--- src/main/java/com/generation/mapaendemico/service/ParqueService.java
+++ src/main/java/com/generation/mapaendemico/service/ParqueService.java
@@ -1,7 +1,16 @@
 package com.generation.mapaendemico.service;
 
+import com.generation.mapaendemico.models.Parque;
+import com.generation.mapaendemico.repository.ParqueRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ParqueService {
+    @Autowired
+    private ParqueRepository parqueRepository;
+
+    public Parque getParqueById(Integer id) {
+        return ParqueRepository.getReferenceById(id);
+    }
 }
