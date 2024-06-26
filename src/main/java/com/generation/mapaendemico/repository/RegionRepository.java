240668d5f5782e72551da1edc2f5f8aa8261fc57
RemiAspergay <168590648+RemiAspergay@users.noreply.github.com>
2024-06-26 15:14:25 -0400

    cambios en region: controller, dto, service y repository, con operaciones crud

--- src/main/java/com/generation/mapaendemico/repository/RegionRepository.java
+++ src/main/java/com/generation/mapaendemico/repository/RegionRepository.java
@@ -6,7 +6,10 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RegionRepository extends JpaRepository<Region, Integer> {
-
+    
+    Region findById(Long id);
     Region findByNombre(String nombre);
     Region findByNumero(int numero);
+
+    Region getReferenceById(Long id);
 }
