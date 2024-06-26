a97ca9870a19c92ee774ceb1335c8014efd18727
Paula <paula.vargas.cist@gmail.com>
2024-06-26 08:21:37 -0600

    3 commit Pau

--- src/main/java/com/generation/mapaendemico/controller/ParqueController.java
+++ src/main/java/com/generation/mapaendemico/controller/ParqueController.java
@@ -1,13 +1,47 @@
 package com.generation.mapaendemico.controller;
 
+import com.generation.mapaendemico.dto.ParqueDTO;
+import com.generation.mapaendemico.models.Parque;
 import com.generation.mapaendemico.service.ParqueService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
 
 @RestController
 @RequestMapping("api/parques")
 public class ParqueController {
     @Autowired
     private ParqueService parqueService;
+    @GetMapping("/nombre")
+    public String getParqueByNombre(@PathVariable String nombre){
+        Parque parqueSolicitado = parqueService.findByNombre(nombre);
+        return parqueSolicitado.getNombre();
+    }
+
+
+    @PostMapping("/nuevoParque")
+    public Parque createParque(@RequestBody Parque parque) {
+        return parqueService.createParque(parque);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<?> updateParqueByName(@PathVariable String nombre, @RequestBody
+                                                     @Valid ParqueDTO parqueParaActualizar, BindingResult result) {
+        if (result.hasErrors()) {
+            return new ResponseEntity<>("Verifica los campos antes de actualizar", HttpStatus.NOT_ACCEPTABLE);
+        }
+        return new ResponseEntity<>(parqueService.updateParqueByName(parqueParaActualizar, nombre),
+                HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/nombre")
+    public ResponseEntity<?>deleteByNombre(@PathVariable String nombre) {
+        parqueService.deleteByNombre(nombre);
+        return new ResponseEntity<>("Parque borrado exitosamente", HttpStatus.OK);
+    }
 }
--- src/main/java/com/generation/mapaendemico/service/ParqueService.java
+++ src/main/java/com/generation/mapaendemico/service/ParqueService.java
@@ -1,16 +1,43 @@
 package com.generation.mapaendemico.service;
 
+import com.generation.mapaendemico.dto.ParqueDTO;
 import com.generation.mapaendemico.models.Parque;
 import com.generation.mapaendemico.repository.ParqueRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class ParqueService {
     @Autowired
     private ParqueRepository parqueRepository;
 
-    public Parque getParqueById(Integer id) {
-        return ParqueRepository.getReferenceById(id);
+    public Parque findById(int id) {
+        return parqueRepository.findById(id);
     }
+    @Transactional
+    public Parque findByNombre(String nombre) {
+        return parqueRepository.findByNombre(nombre);
+    }
+
+    @Transactional
+    public Parque createParque(Parque parque) {
+        return parqueRepository.save(parque);
+    }
+
+    @Transactional
+    public ParqueDTO updateParqueByName(ParqueDTO parqueActualizado, String nombre) {
+        Parque parqueActualizar = parqueRepository.findByNombre(nombre);
+        parqueActualizar.setNombre(parqueActualizado.getNombreParque());
+        parqueActualizar.setDescripcion(parqueActualizado.getDescripcionParque());
+        parqueRepository.save(parqueActualizar);
+        return parqueActualizado;
+    }
+
+    @Transactional
+    public void deleteByNombre(String nombre) {
+        parqueRepository.deleteByNombre(nombre);
+    }
+
 }
